a0ae3730fdcc1ed9b3eed334140f19a69d030a1f
ggian123 <lautinwa9999@gmail.com>
2023-08-05 15:29:31 +0800

    first commit

---
+++ PAT1010.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int arr[10000];
+bool cmp(int a, int b) {return a > b;}
+bool isprime(int a) {
+    for (int i = 2; i * i <= a; i++)
+        if (a % i == 0) return false;
+    return true;
+}
+int main() {
+    int x, p;
+    int flag = 0;
+    while(cin >> x >> p){
+        if (p != 0) {
+            if (flag == 1) {
+                cout << " ";
+            }
+            cout << (x * p) << " " << p - 1;
+            flag = 1;
+        }
+        if (flag == 0) {
+            cout << "0 0";
+        }
+    }
+
+}
