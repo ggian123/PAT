fb919b31090587c69ef7faebd567036ae1165ca5
ggian123 <lautinwa9999@gmail.com>
2023-08-17 14:41:14 +0800

    20230817

---
+++ PAT1040.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int len = s.length(), result = 0, countp = 0, countt = 0;
+    for (int i = 0; i < len; i++) {
+        if (s[i] == 'T')
+            countt++;
+    }
+    for (int i = 0; i < len; i++) {
+        if (s[i] == 'P') countp++;
+        if (s[i] == 'T') countt--;
+        if (s[i] == 'A') result = (result + (countp * countt)) % 1000000007;
+    }
+    cout << result % 1000000007;
+
+}
