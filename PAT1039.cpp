93b2a6fa667cd7e7a61f6efad4edce8b19a8cc41
ggian123 <lautinwa9999@gmail.com>
2023-08-17 14:25:10 +0800

    20230817

---
+++ PAT1039.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int book[256] = {0};
+    string a, b;
+    cin >> a >> b;
+    for (int i = 0; i < a.length(); i++)
+        book[a[i]]++;
+    int result = 0;
+    for (int i = 0; i < b.length(); i++) {
+        if (book[b[i]] > 0)
+            book[b[i]]--;
+        else
+            result++;
+    }
+    if(result != 0)
+        printf("No %d", result);
+    else
+        printf("Yes %d", a.length() - b.length());
+    return 0;
+
+}
