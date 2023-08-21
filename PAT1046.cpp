fcd5ec3291987a976e94f6d7b35f47d36725b5e0
ggian123 <lautinwa9999@gmail.com>
2023-08-22 06:55:21 +0800

    20230822

--- PAT1046.cpp
+++ PAT1046.cpp
@@ -5,18 +5,18 @@ int n, m, cnt;
 string temp, smallx = "99999999", smalla = "99999999", ansx, ansa;
 set<string> record;
 int main() {
-    int n, a1, a2, b1, b2;
-    int al = 0, bl = 0;
+    int n, t, num, score;
     cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> a1 >> a2 >> b1 >> b2;
-        if (a2 == a1 + b1 && b2 != a1 + b1) {
-            bl++;
-        }
-        else if (a2 != a1 + b1 && b2 == a1 + b1) {
-            al++;
-        }
+    int team[1001] = {0};
+    for (int i = 1; i <= n; i++) {
+        scanf("%d-%d %d", &t, &num, &score);
+        team[t] += score;
     }
-    cout << al << " " << bl;
-    
+    int max = 0;
+    for (int i = 0; i < 1001; i++) {
+        if (team[max] < team[i])
+            max = i;
+    }
+    cout << max << " " << team[max];
+    return 0;
 }
